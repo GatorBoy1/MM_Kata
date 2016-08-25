3fe4484d0d63c484b2ca38681e32d0ef95af1073
Don Kingery <Don Kingery>
2016-08-25 12:49:53 -0400

    2nd Test

--- test_mined_mines_function.rb
+++ test_mined_mines_function.rb
@@ -4,4 +4,11 @@ class TestMinedMindsFunction <Minitest::Test
 	def test_1_equal_1 
 		assert_equal(1, mined_minds(1))
 	end
+
+	def test_2_equal_2 
+		assert_equal(2, mined_minds(2))
+	end
+
+
+
 end
