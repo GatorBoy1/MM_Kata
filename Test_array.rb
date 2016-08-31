2e4580eede7d54132dd4eac9be2cf42278701e4b
Don Kingery <Don Kingery>
2016-08-31 15:47:34 -0400

    100_array_1st_commit

---
+++ Test_array.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "array_func.rb"
+class TestArrayFunction <Minitest::Test
+	def test_array_with_100_elements
+		results = array_mined_minds
+		assert_equal(100, results.length)
+
+	end
+
+
+
+end 
