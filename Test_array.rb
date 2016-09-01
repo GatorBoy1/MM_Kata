c7fe9adec5b0f32f1da234716100cfa6977412ca
Don Kingery <Don Kingery>
2016-09-01 12:41:58 -0400

    Array_Test_Replace_15with_Mined_Minds

--- Test_array.rb
+++ Test_array.rb
@@ -10,19 +10,13 @@ class TestArrayFunction <Minitest::Test
 
 		assert_equal(100, results.length)
 
-
-
 	end
-
-
 
 	def test_3_returns_mined
 
 		results = array_mined_minds
 
 		assert_equal("mined" , results[2])
-
-
 
 	end
 
@@ -33,10 +27,16 @@ def test_5_returns_mined
 		assert_equal("minds" , results[4])
 
 
-
 	end
 
+	def test_15_returns_mined_minds
 
+		results = array_mined_minds
+
+		assert_equal("mined minds" , results[14])
+
+
+	end
 
 
 end 
--- array_func.rb
+++ array_func.rb
@@ -1,14 +1,19 @@
 def array_mined_minds
 	my_array = []
 	(1..100).each do |number|
-	if number == 3
-		number = "mined"
+	if number == 15
+		number = "mined minds"
 	
-elsif number == 5
-		number = "minds"	
 
-	else
-	number = number
+	elsif number == 3
+		  number = "mined"
+
+
+	
+	elsif  number == 5
+		   number = "minds"	
+
+	else number = number
 	end
 	my_array << number	
 	end
